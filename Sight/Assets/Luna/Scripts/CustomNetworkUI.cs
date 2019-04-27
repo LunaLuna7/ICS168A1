117d231384f11a67bdefd952b3ca1ae1ab37e908
Luis Luna <lfluna@uci.edu>
2019-04-27 15:14:19 -0700

    JUICE and own scene UI

---
+++ Sight/Assets/Luna/Scripts/CustomNetworkUI.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CustomNetworkUI : NetworkManager
+{
+    public void StartupHost()
+    {
+        SetPort();
+        NetworkManager.singleton.StartHost();
+        SceneManager.LoadScene(0);
+    }
+
+    public void JoinGame()
+    {
+        SetIPAddress();
+        SetPort();
+        NetworkManager.singleton.StartClient();
+
+    }
+
+    public void SetIPAddress()
+    {
+        string ipAdress = GameObject.Find("InputFieldAdress").transform.FindChild("Text").GetComponent<Text>().text;
+        NetworkManager.singleton.networkAddress = ipAdress;
+    }
+
+    void SetPort()
+    {
+        NetworkManager.singleton.networkPort = 7777;
+    }
+
+    private void OnLevelWasLoaded(int level)
+    {
+        if(level == 0)
+        {
+            SetupMenuSceneButtons();
+        }
+        else
+        {
+            SetUpOtherSceneButtons();
+        }
+    }
+
+    void SetupMenuSceneButtons()
+    {
+        GameObject.Find("ButtonStartHost").GetComponent<Button>().onClick.RemoveAllListeners();
+        GameObject.Find("ButtonStartHost").GetComponent<Button>().onClick.AddListener(StartupHost);
+
+        GameObject.Find("ButtonJoinHost").GetComponent<Button>().onClick.RemoveAllListeners();
+        GameObject.Find("ButtonJoinHost").GetComponent<Button>().onClick.AddListener(StartupHost);
+    }
+
+    void SetUpOtherSceneButtons()
+    {
+        GameObject.Find("ButtonDisconnect").GetComponent<Button>().onClick.RemoveAllListeners();
+        GameObject.Find("ButtonDisconnect").GetComponent<Button>().onClick.AddListener(NetworkManager.singleton.StopHost);
+    }
+}
